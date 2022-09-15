bf5ae59290325e31dc055dac57563938a40ac1e1
Devesh Kumar <devesh2027@gmail.com>
2022-09-15 22:04:24 +0530

    feat: Added error boundary for security rules

--- library/express/src/securityRules/isAllowedBySecurityRules.ts
+++ library/express/src/securityRules/isAllowedBySecurityRules.ts
@@ -19,56 +19,60 @@ const isAllowedBySecurityRules = async (
 	args: SecurityRulesCheckerArgs,
 	securityRulesObject: SecurityRules
 ) => {
-	const { req, collection, id, newResource, resource, filters, operation } =
-		args;
+	try {
+		const { req, collection, id, newResource, resource, filters, operation } =
+			args;
 
-	const ruleEvaluator = async (decider: SecurityRulesDecider) => {
-		if (typeof decider === "boolean") return decider;
-		if (typeof decider === "function")
-			return (
-				(await decider({
-					req: req || ({} as Request),
-					collection,
-					id,
-					newResource,
-					resource,
-					filters,
-					operation,
-				})) || false
-			);
-		return false;
-	};
+		const ruleEvaluator = async (decider: SecurityRulesDecider) => {
+			if (typeof decider === "boolean") return decider;
+			if (typeof decider === "function")
+				return (
+					(await decider({
+						req: req || ({} as Request),
+						collection,
+						id,
+						newResource,
+						resource,
+						filters,
+						operation,
+					})) || false
+				);
+			return false;
+		};
 
-	// If no security rules, let everything pass.
-	if (!securityRulesObject) return true;
+		// If no security rules, let everything pass.
+		if (!securityRulesObject) return true;
 
-	// Get the most granular collection-level rules out of the way.
-	const collectionLevelRules = securityRulesObject[collection];
-	if (collectionLevelRules && typeof collectionLevelRules === "object") {
-		const collectionLevelOpRule = collectionLevelRules[operation];
-		if (operation in collectionLevelRules)
-			return await ruleEvaluator(collectionLevelOpRule);
-		else {
-			// Check for less-granular collection-level rules like 'read', 'write'
-			if (readOps.includes(operation) && "read" in collectionLevelRules)
-				return await ruleEvaluator(collectionLevelRules.read);
-			if (writeOps.includes(operation) && "write" in collectionLevelRules)
-				return await ruleEvaluator(collectionLevelRules.write);
+		// Get the most granular collection-level rules out of the way.
+		const collectionLevelRules = securityRulesObject[collection];
+		if (collectionLevelRules && typeof collectionLevelRules === "object") {
+			const collectionLevelOpRule = collectionLevelRules[operation];
+			if (operation in collectionLevelRules)
+				return await ruleEvaluator(collectionLevelOpRule);
+			else {
+				// Check for less-granular collection-level rules like 'read', 'write'
+				if (readOps.includes(operation) && "read" in collectionLevelRules)
+					return await ruleEvaluator(collectionLevelRules.read);
+				if (writeOps.includes(operation) && "write" in collectionLevelRules)
+					return await ruleEvaluator(collectionLevelRules.write);
+			}
 		}
-	}
 
-	// Less granular
-	if (operation in securityRulesObject) {
-		const rootLevelOpRule = securityRulesObject[operation];
-		return await ruleEvaluator(rootLevelOpRule);
-	} else {
-		if (readOps.includes(operation) && "read" in securityRulesObject)
-			return await ruleEvaluator(securityRulesObject.read);
-		if (writeOps.includes(operation) && "write" in securityRulesObject)
-			return await ruleEvaluator(securityRulesObject.write);
-	}
+		// Less granular
+		if (operation in securityRulesObject) {
+			const rootLevelOpRule = securityRulesObject[operation];
+			return await ruleEvaluator(rootLevelOpRule);
+		} else {
+			if (readOps.includes(operation) && "read" in securityRulesObject)
+				return await ruleEvaluator(securityRulesObject.read);
+			if (writeOps.includes(operation) && "write" in securityRulesObject)
+				return await ruleEvaluator(securityRulesObject.write);
+		}
 
-	return false;
+		return false;
+	} catch {
+		return false;
+	}
 };
 
 export default isAllowedBySecurityRules;
