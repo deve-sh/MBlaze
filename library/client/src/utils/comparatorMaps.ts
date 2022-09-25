0c6949a8cdb340f426f3533581b65a39efed0640
Devesh Kumar <devesh2027@gmail.com>
2022-09-26 00:11:17 +0530

    feat: Comparator map function created for creating filters

---
+++ library/client/src/utils/comparatorMaps.ts
@@ -0,0 +1,19 @@
+import comparatorType from "../types/comparator";
+
+const comparatorMap = (
+	comparator: comparatorType,
+	value: any
+): Record<string, any> | any => {
+	if (comparator === "==") return { $eq: value };
+	if (comparator === "!=") return { $ne: value };
+	if (comparator === "<=") return { $lte: value };
+	if (comparator === ">=") return { $gte: value };
+	if (comparator === "array-contains") return value; // MongoDB auto checks { field: value } for array membership.
+	if (comparator === "array-contains-any")
+		return { $elemMatch: comparatorMap("in", value) };
+	if (comparator === "in") return { $in: Array.isArray(value) ? value : [] };
+	if (comparator === "not-in")
+		return { $nin: Array.isArray(value) ? value : [] };
+};
+
+export default comparatorMap;
