1194f17f04aeb2d89cf2813f6e45a0e6024d7e85
Devesh Kumar <devesh2027@gmail.com>
2022-09-25 21:05:23 +0530

    feat: Added deletion of existing dist directory before build and publish

--- library/express/scripts/prepublish.js
+++ library/express/scripts/prepublish.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const { execSync } = require("child_process");
 
+fs.rmSync("./dist", { recursive: true, force: true });
 execSync("npm run compile", { stdio: "inherit" });
 fs.copyFileSync("./package.json", "./dist/package.json");
 fs.copyFileSync("./README.md", "./dist/README.md");
