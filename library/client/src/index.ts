c3e90d68040161929588295e9558f8b5eeb1c19b
Devesh Kumar <devesh2027@gmail.com>
2022-09-25 12:09:34 +0530

    feat: Removed FetchedDoc and FetchedCollection Classes from exports

--- library/client/src/index.ts
+++ library/client/src/index.ts
@@ -1,9 +1,7 @@
 import DB from "./classes/DB";
 import CollectionRef from "./classes/Collection";
 import DocRef from "./classes/DocRef";
-import FetchedCollection from "./classes/FetchedCollection";
-import FetchedDoc from "./classes/FetchedDoc";
 
 export default DB;
 
-export { DB, CollectionRef, DocRef, FetchedDoc, FetchedCollection };
+export { DB, CollectionRef, DocRef };
