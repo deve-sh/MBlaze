7b609d7c111e677cc36898d02136a2851cc40b8d
Devesh Kumar <devesh2027@gmail.com>
2022-09-26 09:53:48 +0530

    feat: Removed unnecessary exports for client SDK

--- library/client/src/index.ts
+++ library/client/src/index.ts
@@ -1,7 +1,2 @@
 import DB from "./classes/DB";
-import CollectionRef from "./classes/CollectionClasses/Collection";
-import DocRef from "./classes/DocRef";
-
 export default DB;
-
-export { DB, CollectionRef, DocRef };
