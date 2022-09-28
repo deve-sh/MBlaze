589e01bec976324fefe18bbf24b65ab5f4d490e3
Devesh Kumar <devesh2027@gmail.com>
2022-09-28 16:48:11 +0530

    feat: Added FieldValue export to client Lib

--- library/client/src/index.ts
+++ library/client/src/index.ts
@@ -1,2 +1,15 @@
 import DB from "./classes/DB";
 export default DB;
+
+// Reserved op types/field types
+import arrayUnion from "./reservedFieldTypes/arrayUnion";
+import arrayRemove from "./reservedFieldTypes/arrayRemove";
+import serverTimestamp from "./reservedFieldTypes/serverTimestamp";
+import increment from "./reservedFieldTypes/increment";
+
+export const FieldValue = {
+	arrayUnion,
+	arrayRemove,
+	increment,
+	serverTimestamp,
+};
