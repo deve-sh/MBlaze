612fced2d01a224709423c5c8bb7c89f2892ccf1
Devesh Kumar <devesh2027@gmail.com>
2022-09-28 17:09:46 +0530

    feat: Added considerTypes for reserved field types mapper

--- library/express/src/reservedFieldTypes/modifyObjectForReservedTypes.ts
+++ library/express/src/reservedFieldTypes/modifyObjectForReservedTypes.ts
@@ -11,29 +11,48 @@ import arrayUnion, {
 
 const modifyObjectForReservedFieldTypes = (
 	obj: Record<string, any>,
+	considerTypes = [
+		// Only server timestamp supported in case of set operations
+		SERVER_TIMESTAMP_TYPE,
+		INCREMENT_TYPE,
+		ARRAY_REMOVE_TYPE,
+		ARRAY_UNION_TYPE,
+	],
 	rootObj?: Record<string, any>
 ) => {
 	if (!rootObj) rootObj = obj;
 
 	for (let key in obj) {
 		if (obj.hasOwnProperty(key)) {
-			if (obj[key]?.type === SERVER_TIMESTAMP_TYPE)
+			if (
+				obj[key]?.type === SERVER_TIMESTAMP_TYPE &&
+				considerTypes.includes(SERVER_TIMESTAMP_TYPE)
+			)
 				obj[key] = new Date(); // Server Timestamp
-			else if (obj[key]?.type === INCREMENT_TYPE) {
+			else if (
+				obj[key]?.type === INCREMENT_TYPE &&
+				considerTypes.includes(INCREMENT_TYPE)
+			) {
 				if (rootObj[INCREMENT_OP_CODE])
 					rootObj[INCREMENT_OP_CODE] = {
 						...rootObj[INCREMENT_OP_CODE],
 						...increment(key, obj[key].by),
 					};
 				else rootObj[INCREMENT_OP_CODE] = increment(key, obj[key].by);
-			} else if (obj[key]?.type === ARRAY_UNION_TYPE) {
+			} else if (
+				obj[key]?.type === ARRAY_UNION_TYPE &&
+				considerTypes.includes(ARRAY_UNION_TYPE)
+			) {
 				if (rootObj[ARRAY_UNION_OP_CODE])
 					rootObj[ARRAY_UNION_OP_CODE] = {
 						...rootObj[ARRAY_UNION_OP_CODE],
 						...arrayUnion(key, obj[key].toAdd),
 					};
 				else rootObj[ARRAY_UNION_OP_CODE] = arrayUnion(key, obj[key].toAdd);
-			} else if (obj[key]?.type === ARRAY_REMOVE_TYPE) {
+			} else if (
+				obj[key]?.type === ARRAY_REMOVE_TYPE &&
+				considerTypes.includes(ARRAY_REMOVE_TYPE)
+			) {
 				if (rootObj[ARRAY_REMOVE_OP_CODE])
 					rootObj[ARRAY_REMOVE_OP_CODE] = {
 						...rootObj[ARRAY_REMOVE_OP_CODE],
@@ -42,7 +61,7 @@ const modifyObjectForReservedFieldTypes = (
 				else
 					rootObj[ARRAY_REMOVE_OP_CODE] = arrayRemove(key, obj[key].toRemove);
 			} else if (obj[key] && typeof obj[key] === "object")
-				modifyObjectForReservedFieldTypes(obj[key], rootObj);
+				modifyObjectForReservedFieldTypes(obj[key], considerTypes, rootObj);
 		}
 	}
 
