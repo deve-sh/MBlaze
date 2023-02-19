1d8cc9961aa4c0912bc5b51ef980b41c24a1b95b
Devesh Kumar <devesh2027@gmail.com>
2023-02-19 18:04:34 +0530

    Client Lib: Cleaned up reties and fallback URL checks

--- library/client/tests/retries.test.ts
+++ library/client/tests/retries.test.ts
@@ -4,9 +4,9 @@ import { describe, test, expect } from "@jest/globals";
 import DB from "../src";
 
 describe("Fallback URL Config Tests", () => {
-	const db = new DB("http://localhost:8081/mongodb", {
-		fallbackURL: "http://localhost:8080/mongodb",
-	});
+	const invalidURL = "http://localhost:8081/mongodb";
+	const validURL = "http://localhost:8080/mongodb";
+	const db = new DB(invalidURL, { fallbackURL: validURL });
 
 	test("should use fallback url inc ase an invalid primary URL or failed primary url is passed", async () => {
 		const data = await db.collection("projects").doc("abc").get();
